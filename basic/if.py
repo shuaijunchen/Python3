cf5c0e94e3694955457acac62575dec2ce1b7ae1
Charles Chen <toshuaijun@gmail.com>
2017-09-19 20:27:43 +0800

    elif and else 使用

--- basic/if.py
+++ basic/if.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-age = 20
+age = 3
 if age >= 18:
-	print('your age is',age)
+	print('your age is', age)
 	print('adult')
+elif age >= 6:
+	print('teenager')
+else:
+	print('kid')
