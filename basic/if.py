0f6f8f85cb3b834b8da5f5d2a07c11fec9a6e276
Charles Chen <toshuaijun@gmail.com>
2017-09-19 20:35:30 +0800

    只要x是非零值、非空字符串、非空list等，就为True否则为Flase

--- basic/if.py
+++ basic/if.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-age = 3
-if age >= 18:
-	print('your age is', age)
-	print('adult')
-elif age >= 6:
-	print('teenager')
+x = 'str'
+if x:
+	print('True')
 else:
-	print('kid')
+	print('Flase')
