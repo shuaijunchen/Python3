7070b93cfbe31253fcbbb94af16e87873d22388d
Charles Chen <toshuaijun@gmail.com>
2017-09-22 21:52:45 +0800

    更新函数计算平方

--- basic/func2.py
+++ basic/func2.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def power(x):
-	return x*x
+def power(x, n):
+	s = 1
+	while n > 0:
+		n = n - 1
+		s = s * x
+	return s
 
-print(power(5), power(15))
+print(power(5, 2), power(5, 3))
