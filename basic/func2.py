b063d4de2610a52e32bf5a5b26d7e09efb0e9e04
Charles Chen <toshuaijun@gmail.com>
2017-09-22 21:56:57 +0800

    设置函数默认值

--- basic/func2.py
+++ basic/func2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def power(x, n):
+def power(x, n = 2):
 	s = 1
 	while n > 0:
 		n = n - 1
 		s = s * x
 	return s
 
-print(power(5, 2), power(5, 3))
+print(power(5), power(5, 3))
