11bbc24de294216fc38c83553e747d029833d51a
Charles Chen <toshuaijun@gmail.com>
2017-09-22 21:28:29 +0800

    数据类型判断

--- basic/func.py
+++ basic/func.py
@@ -2,14 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def my_abs(x):
+	if not isinstance(x, (int, float)):
+		raise TypeError('bad operand type')
 	if x >= 0:
 		return x
 	else:
 		return -x
 
-print(my_abs(-10))
-
-def nop():
-	pass
-
-print(nop())
+print(my_abs('a'))
