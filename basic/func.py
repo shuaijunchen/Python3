5aae89257166204615b14b31bf99c03abf58c43f
Charles Chen <toshuaijun@gmail.com>
2017-09-22 21:44:18 +0800

    Python返回参数

--- basic/func.py
+++ basic/func.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import math
+
 def my_abs(x):
 	if not isinstance(x, (int, float)):
 		raise TypeError('bad operand type')
@@ -9,4 +11,14 @@ def my_abs(x):
 	else:
 		return -x
 
-print(my_abs('a'))
+# print(my_abs('a'))
+
+def move(x, y, step, angle=0):
+	nx = x + step * math.cos(angle)
+	ny = y - step * math.sin(angle)
+	return nx, ny
+
+# x, y = move(10, 10, 6, math.pi/0.6)
+# print(x,y)
+x = move(10, 10, 6, math.pi/0.6)
+print(x)
