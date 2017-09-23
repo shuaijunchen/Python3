7d71bf4b26b0b4290612494b99d5d006f584807a
Charles Chen <toshuaijun@gmail.com>
2017-09-23 21:13:06 +0800

    使用os.listdir列出目录

--- basic/listcomper.py
+++ basic/listcomper.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 L = list(range(1,11))
 print(L)
 
@@ -13,3 +15,5 @@ print(L)
 print([x * x for x in range(1, 11) if x % 2 == 0])
 
 print([m+n for m in 'ABC' for n in 'XYZ'])
+
+print([d for d in os.listdir('.')])
