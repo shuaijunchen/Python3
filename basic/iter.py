379db588faa89391d9dd9808576920565a9394cb
Charles Chen <toshuaijun@gmail.com>
2017-09-23 22:32:26 +0800

    判断一个对象是否是迭代器对象

--- basic/iter.py
+++ basic/iter.py
@@ -4,8 +4,15 @@
 from collections import Iterable
 from collections import Iterator
 
+# Iterable
 print(isinstance([], Iterable)) # list
 print(isinstance({}, Iterable)) # dict
 print(isinstance('abc', Iterable)) # str
 print(isinstance((x for x in range(10)), Iterable))
 print(isinstance(100, Iterable)) # int
+
+# Iterator
+print(isinstance((x for x in range(10)), Iterator))
+print(isinstance([], Iterator))
+print(isinstance({}, Iterator))
+print(isinstance('abc', Iterator))
