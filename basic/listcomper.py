fc114569c07228a5b6d06306a117e526b93178c7
Charles Chen <toshuaijun@gmail.com>
2017-09-23 21:16:29 +0800

    for遍历dict的items()可以迭代key、

--- basic/listcomper.py
+++ basic/listcomper.py
@@ -17,3 +17,6 @@ print([x * x for x in range(1, 11) if x % 2 == 0])
 print([m+n for m in 'ABC' for n in 'XYZ'])
 
 print([d for d in os.listdir('.')])
+
+d = {'x':'A', 'y':'B', 'z':'C'}
+print([k + '=' + v for k, v in d.items()])
