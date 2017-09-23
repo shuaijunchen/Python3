a337eedc19c769df240df33d87216cb992f8f77f
Charles Chen <toshuaijun@gmail.com>
2017-09-23 20:38:57 +0800

    判断一个对象是否可以迭代

--- basic/iteration.py
+++ basic/iteration.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from collections import Iterable
 
 d = {'a':1, 'b':2, 'c':3}
 for k,v in d.items():
@@ -7,3 +8,8 @@ for k,v in d.items():
 
 for ch in 'ABC':
     print(ch)
+
+# 判断一个str是否可迭代
+print(isinstance('abc', Iterable)) # str
+print(isinstance([1,2,3], Iterable)) # list
+print(isinstance(123, Iterable)) # int
