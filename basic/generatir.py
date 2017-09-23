1c1944c1225e91abc32ee1938d11d5c244983b59
Charles Chen <toshuaijun@gmail.com>
2017-09-23 22:14:37 +0800

    while 遍历genaratir

--- basic/generatir.py
+++ basic/generatir.py
@@ -15,9 +15,32 @@ for n in g:
 def fib(max):
     n, a, b = 0, 0, 1
     while n < max:
-        print(b)
+        yield(b)
         a, b = b, a + b
         n = n + 1
     return 'done'
 
-print(fib(6))
+for n in fib(6):
+    print(n)
+
+g = fib(6)
+while True:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield(3)
+    print('step 3')
+    yield(5)
+
+o = odd()
+print(next(o))
+print(next(o))
+print(next(o))
