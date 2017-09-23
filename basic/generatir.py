6756bc1fb065d9fd7c365319a4b8732129a84f6a
Charles Chen <toshuaijun@gmail.com>
2017-09-23 22:03:29 +0800

    斐波拉契数列

--- basic/generatir.py
+++ basic/generatir.py
@@ -11,3 +11,13 @@ print(next(g))
 
 for n in g:
     print(n)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+print(fib(6))
