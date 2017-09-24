ff4c07dd9dec6db4cdfb7e075785a0ad40fb2a81
Charles Chen <toshuaijun@gmail.com>
2017-09-24 16:01:30 +0800

    埃拉托色尼筛选法

--- basic/filter.py
+++ basic/filter.py
@@ -10,3 +10,26 @@ def not_empty(s):
     return s and s.strip()
 
 print(list(filter(not_empty, ['A','','B', None, 'C', '  '])))
+
+def _odd_iter():
+    n = 1
+    while True:
+        n = n + 2
+        yield n
+
+def _not_divisible(n):
+    return lambda x:x % n > 0
+
+def primes():
+    yield 2
+    it = _odd_iter()
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n), it)
+
+for n in primes():
+    if n < 100:
+        print(n)
+    else:
+        break
