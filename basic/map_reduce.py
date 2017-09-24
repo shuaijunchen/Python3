02c983a36f3759ccf5fd1a73d206c5653d517073
Charles Chen <toshuaijun@gmail.com>
2017-09-24 15:26:09 +0800

    reduce 使用

--- basic/map_reduce.py
+++ basic/map_reduce.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
 def f(x):
     return x * x
 
@@ -9,3 +11,9 @@ print(list(r))
 
 # int[] to str[]
 print(list(map(str, [1, 2, 3, 4, 5])))
+
+# reduce 使用
+def add(x, y):
+    return x+y
+
+print(reduce(add, [1, 3, 5, 7, 9]))
