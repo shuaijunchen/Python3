de142380f6e92fe3535c5c6c90959f5aec5575e5
Charles Chen <toshuaijun@gmail.com>
2017-09-24 15:39:01 +0800

    使用map和reduce、将字符串转换为int类型

--- basic/map_reduce2.py
+++ basic/map_reduce2.py
@@ -3,10 +3,10 @@
 
 from functools import reduce
 
-def fn(x, y):
-    return x * 10 + y
-
-def char2num(s):
-    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9' : 9}[s]
-
-print(reduce(fn, map(char2num, '13579')))
+def str2int(s):
+    def fn(x, y):
+        return x * 10 + y
+    def char2num(s):
+        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9' : 9}[s]
+    return reduce(fn, map(char2num, s))
+print(str2int('13579'))
