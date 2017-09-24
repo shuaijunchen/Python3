3844a1e210101764b6023a480f0d9dff18d945df
Charles Chen <toshuaijun@gmail.com>
2017-09-24 15:42:48 +0800

    将str转换为int

--- basic/map_reduce2.py
+++ basic/map_reduce2.py
@@ -3,10 +3,10 @@
 
 from functools import reduce
 
+def char2num(s):
+    return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+
 def str2int(s):
-    def fn(x, y):
-        return x * 10 + y
-    def char2num(s):
-        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9' : 9}[s]
-    return reduce(fn, map(char2num, s))
-print(str2int('13579'))
+    return reduce(lambda x, y: x * 10 + y, map(char2num, s))
+
+print(str2int('2321'))
