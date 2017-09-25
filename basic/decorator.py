42bf17f95bdd119351579767923d8387655745a7
Charles Chen <toshuaijun@gmail.com>
2017-09-25 21:40:38 +0800

    functools.wraps 使用

--- basic/decorator.py
+++ basic/decorator.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import functools
+
 def log(text):
     def decorator(func):
+        @functools.wraps(func)
         def wrapper(*args, **kw):
             print('%s %s():' % (text, func.__name__))
             return func(*args, **kw)
