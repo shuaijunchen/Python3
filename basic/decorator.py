66a73a3e292514f099354bf96a108160e32f2115
Charles Chen <toshuaijun@gmail.com>
2017-09-25 21:34:40 +0800

    装饰器

---
+++ basic/decorator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2017-09-25')
+
+now()
