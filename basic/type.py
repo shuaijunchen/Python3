ed7d7ddc4109fddbc8bc1a6adba3f940d88c55bf
Charles Chen <toshuaijun@gmail.com>
2017-10-08 21:29:06 +0800

    Python 类型比较

--- basic/type.py
+++ basic/type.py
@@ -9,3 +9,11 @@ print(type('abc'))
 print(type(None))
 print(type(abs))
 print(type(a()))
+
+
+# Type comparison
+print(type(123)== type(456))
+print(type(123)== int)
+print(type('str')== type('abc'))
+print(type('123')== str)
+print(type('123')== type(123))
