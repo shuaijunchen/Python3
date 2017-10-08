3cc0353a0be52cd2dd35ae8b93886dc68b5863a4
Charles Chen <toshuaijun@gmail.com>
2017-10-08 21:45:55 +0800

    isinstance()判断的是一个对象是否是该类型本身，或者位于该类型的父继承链上

---
+++ basic/isinstance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+	def run(self):
+		print('Animal is running...')
+
+class Dog(Animal):
+	def run(self):
+		print('Dog is running...')
+
+class Husky(Dog):
+	def run(self):
+		print('Husky is running...')
+
+a = Animal()
+d = Dog()
+h = Husky()
+
+print(isinstance(h, Husky))
+print(isinstance(h, Dog))
+print(isinstance(h, Animal))
+print(isinstance(d, Dog) and isinstance(d, Animal))
+print(isinstance(d, Husky))
