39de08d4b497246511d208144c576ce01548856c
Charles Chen <toshuaijun@gmail.com>
2017-10-08 22:08:23 +0800

    配合getattr()、setattr()以及hasattr()，我们可以直接获取操作一个对象的状态

--- basic/len.py
+++ basic/len.py
@@ -11,3 +11,19 @@ class MyDog(object):
 dog = MyDog()
 
 print(len(dog))
+
+class MyObject(object):
+	def __init__(self):
+		self.x = 9
+	def power(self):
+		return self.x * self.x
+
+obj = MyObject()
+
+print(hasattr(obj, 'x'))
+print(obj.x)
+print(hasattr(obj, 'y'))
+setattr(obj, 'y', 20)
+print(hasattr(obj, 'y'))
+print(getattr(obj, 'y'))
+print(obj.y)
