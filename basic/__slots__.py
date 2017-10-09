8f30e5ede17e31c20bcfef5071813e6208375119
Charles Chen <toshuaijun@gmail.com>
2017-10-09 20:41:18 +0800

    给set_age()函数绑定一个实例方法

--- basic/__slots__.py
+++ basic/__slots__.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from types import MethodType
+
 class Student(object):
 	pass
+
+def set_age(self, age):
+	self.age = age
 
 s = Student()
 s.name = 'Student'
 print(s.name)
+
+s.set_age = MethodType(set_age, s)
+s.set_age(25)
+print(s.age)
