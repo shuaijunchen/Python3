3e20bbf24cd55a7dd0f4d508a5a68aa3e70a5b1a
Charles Chen <toshuaijun@gmail.com>
2017-10-09 20:27:18 +0800

    实例属性和类属性

--- basic/class_attr.py
+++ basic/class_attr.py
@@ -5,4 +5,14 @@ class Student(object):
 	name = 'Student'
 
 s = Student()
-print(s.name)
+print('s.name: ', s.name)
+
+print('Student name: ', Student.name)
+
+s.name = 'Zhangsan'
+print('s.name: ', s.name)
+
+print('Student name: ', Student.name)
+
+del s.name
+print('del s.name: ', s.name)
