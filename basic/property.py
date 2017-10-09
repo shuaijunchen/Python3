4d267181d89f6dfea13ce2ba9a1d2f28582dadfe
Charles Chen <toshuaijun@gmail.com>
2017-10-09 21:39:03 +0800

    Python内置的@property装饰器就是负责把一个方法变成属性调用，@score.setter负责把一个setter方法变成属性赋值

--- basic/property.py
+++ basic/property.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 class Student(object):
-	def get_score(self):
+	@property
+	def score(self):
 		return self._score
 
-	def set_score(self, value):
+	@score.setter
+	def score(self, value):
 		if not isinstance(value, int):
 			raise ValueError('score must be an integer!')
 		if value < 0 or value > 100:
@@ -13,5 +15,5 @@ class Student(object):
 		self._score = value
 
 s  = Student()
-s.set_score(60)
-print(s.get_score())
+s.score = 60
+print(s.score)
