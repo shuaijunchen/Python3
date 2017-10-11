6ef3f93f1b6b3689cde3e5ea0fea718c45b2f2bb
Charles Chen <toshuaijun@gmail.com>
2017-10-11 21:17:30 +0800

    使用__getitem__()方法传入一个切片对象

--- basic/__iter__.py
+++ basic/__iter__.py
@@ -15,13 +15,28 @@ class Fib(object):
 		return self.a
 
 	def __getitem__(self, n):
-		a, b = 1, 1
-		for x in range(n):
-			a, b = b, a + b
-		return a
+		if isinstance(n, int): # n equest index
+			a, b = 1, 1
+			for x in range(n):
+				a, b = b, a + b
+			return a
+		if isinstance(n, slice): # n equest slice
+			start = n.start
+			stop = n.stop
+			if start is None:
+				start = 0
+			a, b = 1, 1
+			L = []
+			for x in range(stop):
+				if x >= start:
+					L.append(a)
+				a, b = b, a + b
+			return L
 
 for n in Fib():
 	print(n)
 
 f = Fib()
 print(f[10])
+
+print(f[:10:2])
