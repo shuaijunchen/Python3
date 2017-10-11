93a06e42173dba9fc4f6f7f4a06eac7580732326
Charles Chen <toshuaijun@gmail.com>
2017-10-11 21:09:22 +0800

    使用__getitem__()获取下标元素

--- basic/__iter__.py
+++ basic/__iter__.py
@@ -9,10 +9,19 @@ class Fib(object):
 		return self
 
 	def __next__(self):
-		self.a, self.b = self.b, self.a+self.b
+		self.a, self.b = self.b, self.a + self.b
 		if self.a > 10000:
 			raise StopIteration()
 		return self.a
 
+	def __getitem__(self, n):
+		a, b = 1, 1
+		for x in range(n):
+			a, b = b, a + b
+		return a
+
 for n in Fib():
 	print(n)
+
+f = Fib()
+print(f[10])
