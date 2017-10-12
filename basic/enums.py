a48620edc443b1b7e3ff938bff7b465397779f24
Charles Chen <toshuaijun@gmail.com>
2017-10-12 20:55:01 +0800

    @unique装饰器可以帮助我们检查保证没有重复值

--- basic/enums.py
+++ basic/enums.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from enum import Enum
+from enum import Enum, unique
 
 Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
@@ -9,3 +9,13 @@ print(Month.Jan)
 
 for name, member in Month.__members__.items():
 	print(name, "=>", member, ',', member.value)
+
+@unique
+class Weekday(Enum):
+	Sun = 0
+	Mon = 1
+	Tue = 2
+	Wed = 3
+	Thu = 4
+	Fri = 5
+	Sta = 6
