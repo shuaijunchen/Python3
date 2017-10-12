cd668ff2baf4d4ea946e733585e7cea476fe5a16
Charles Chen <toshuaijun@gmail.com>
2017-10-12 20:51:12 +0800

    遍历Month枚举类

--- basic/enums.py
+++ basic/enums.py
@@ -6,3 +6,6 @@ from enum import Enum
 Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
 print(Month.Jan)
+
+for name, member in Month.__members__.items():
+	print(name, "=>", member, ',', member.value)
