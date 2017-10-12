420655f5382a875232e81bb1e3ea95aac42db253
Charles Chen <toshuaijun@gmail.com>
2017-10-12 20:48:10 +0800

    直接使用Month.Jan变量

---
+++ basic/enums.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+print(Month.Jan)
