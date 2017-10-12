29c3c98bfd3047eaec6f84318616ff5fa52bd1d9
Charles Chen <toshuaijun@gmail.com>
2017-10-12 21:27:00 +0800

    except 捕获不同的类型错误

--- basic/error.py
+++ basic/error.py
@@ -5,8 +5,10 @@ try:
 	print('try...')
 	r = 10 / 2
 	print('result:', r)
+except ValueError as e:
+	print('ValueError:', e)
 except ZeroDivisionError as e:
-	print('except:', e)
+	print('ZeroDivisionError:', e)
 finally:
 	print('finally...')
 print('end.')
