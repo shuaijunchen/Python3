55d14f28569a696503cc83a1d6ea60e6c550ccbd
Charles Chen <toshuaijun@gmail.com>
2017-10-13 22:10:48 +0800

    可以用一个str初始化StringIO

--- basic/string_io.py
+++ basic/string_io.py
@@ -9,3 +9,10 @@ print(f.write(' '))
 print(f.write('world!'))
 
 print(f.getvalue())
+
+s = StringIO('Hello\nHi\nGoodbye!')
+while True:
+	string = s.readline()
+	if string == '':
+		break
+	print(string.strip())
