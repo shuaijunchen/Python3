a015c59a747587159b873276eb6fefb3d70dd56e
Charles Chen <toshuaijun@gmail.com>
2017-10-14 19:41:25 +0800

    当多个线程同时执行lock.acquire()时，只有一个线程能成功获取锁，然后继续执行代码，其他线程就继续等待直到获的锁为止

--- basic/do_lock.py
+++ basic/do_lock.py
@@ -5,6 +5,7 @@ import time, threading
 
 # 银行存款
 balance = 0
+lock = threading.Lock() # 锁
 
 def change_it(n):
     # 先存后取，结果应该为0
@@ -14,7 +15,13 @@ def change_it(n):
 
 def run_thread(n):
     for i in range(100000):
-        change_it(n)
+        # 先要获得锁
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            # 改完了一定要释放锁
+            lock.release()
 
 t1 = threading.Thread(target=run_thread, args=(5,))
 t2 = threading.Thread(target=run_thread, args=(8,))
