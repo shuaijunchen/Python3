e9484ff4900119ac641e52d493a9a5594e97d21c
Charles Chen <toshuaijun@gmail.com>
2017-10-14 15:50:06 +0800

    还原

--- basic/do_subprocess.py
+++ basic/do_subprocess.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+	
 import subprocess
-
+	
 print('$ nslookup chenshuaijun.com')
 r = subprocess.call(['nslookup', 'chenshuaijun.com'])
 print('Exit code:', r)
