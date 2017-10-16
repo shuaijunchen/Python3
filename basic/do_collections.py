ea052bfa15ea6183fa89fc2b92cd99656c2961a4
Charles Chen <toshuaijun@gmail.com>
2017-10-16 22:39:05 +0800

    使用dict时，如果引用的key不存在，就会抛出KeyError。如果希望key不存在时，就会返回一个默认值，就可以使用defaultdict

--- basic/do_collections.py
+++ basic/do_collections.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple, deque
+from collections import namedtuple, deque, defaultdict
 
 Point = namedtuple('Point', ['x', 'y'])
 p = Point(1, 2)
@@ -15,3 +15,9 @@ q = deque(['a', 'b', 'c'])
 q.append('x')
 q.appendleft('y')
 print(q)
+
+dd = defaultdict(lambda:'N/A')
+dd['key1'] = 'abc'
+print(dd['key1']) # key1存在
+print(dd['key2']) # key2不存在，返回默认值
+
