af297927f189caf980da6ab335936f327d8800b5
Charles Chen <toshuaijun@gmail.com>
2017-10-16 21:11:46 +0800

    str转换为datetime

--- basic/do_datetime.py
+++ basic/do_datetime.py
@@ -13,3 +13,6 @@ print(dt.timestamp()) # 把datetime转换为timestamp
 t = 1508158860.0
 print(datetime.fromtimestamp(t)) # 本地时间
 print(datetime.utcfromtimestamp(t)) # UTC时间
+
+cday = datetime.strptime('2017-10-16 21:10:29', "%Y-%m-%d %H:%M:%S") # str转换为datetime
+print(cday)
