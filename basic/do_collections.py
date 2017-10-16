3990da9e5e88350a427b7da9d209784b4f4f2a32
Charles Chen <toshuaijun@gmail.com>
2017-10-16 22:43:29 +0800

    使用dict时，key是无序的。在对dict做迭代时，我们无法确定key的顺序;如果要保持key的顺序，可以用OrderedDict

--- basic/do_collections.py
+++ basic/do_collections.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple, deque, defaultdict
+from collections import namedtuple, deque, defaultdict, OrderedDict
 
 Point = namedtuple('Point', ['x', 'y'])
 p = Point(1, 2)
@@ -21,3 +21,8 @@ dd['key1'] = 'abc'
 print(dd['key1']) # key1存在
 print(dd['key2']) # key2不存在，返回默认值
 
+# OrderedDict
+d = dict([('a', 1), ('b', 2), ('c', 3)])
+print(d)
+od = OrderedDict([('a', 2),('b', 2), ('c', 3)])
+print(od)
