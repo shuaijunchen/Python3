5a1b8a5806dd845e01ca1b5430ffdd5ec6af3de5
Charles Chen <toshuaijun@gmail.com>
2017-10-16 22:33:59 +0800

    deque是为了高效实现插入和删除操作的双向列表

--- basic/do_collections.py
+++ basic/do_collections.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple
+from collections import namedtuple, deque
 
 Point = namedtuple('Point', ['x', 'y'])
 p = Point(1, 2)
 print(p.x)
 print(p.y)
+
+print(isinstance(p, Point))
+print(isinstance(p, tuple))
+
+q = deque(['a', 'b', 'c'])
+q.append('x')
+q.appendleft('y')
+print(q)
