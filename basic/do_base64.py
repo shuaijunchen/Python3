328905b4e39527e12fb2dc9f29d2b249cf817848
Charles Chen <toshuaijun@gmail.com>
2017-10-17 21:51:35 +0800

    base64编码可能出现字符+和/，在URL中就不能直接作为参数，所以又有一种"url safe"的base64编码，其实就是把字符+和/分别变成-和_

--- basic/do_base64.py
+++ basic/do_base64.py
@@ -6,3 +6,7 @@ import base64
 print(base64.b64encode(b'python!'))
 print(base64.b64decode(b'cHl0aG9uIQ=='))
 
+print(base64.b64encode(b'i\xb7\x1d\xfb\xef\xff'))
+print(base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff'))
+print(base64.urlsafe_b64decode(b'abcd--__'))
+
