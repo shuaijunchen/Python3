3e96163fedda4180db45d56e50493bc451d35a22
Charles Chen <toshuaijun@gmail.com>
2017-10-19 19:48:25 +0800

    修改一个字符后，摘要算法结果也是不一样的

--- basic/do_hashlib.py
+++ basic/do_hashlib.py
@@ -6,3 +6,9 @@ import hashlib
 md5 = hashlib.md5()
 md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
 print(md5.hexdigest())
+
+md5_ = hashlib.md5()
+md5_.update('how to use md5 in '.encode('utf-8'))
+md5_.update('python hashlib!'.encode('utf-8'))
+print(md5_.hexdigest())
+
