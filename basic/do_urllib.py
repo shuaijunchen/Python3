b3e5a384ce43a84b939bb30e840e61229edeaaad
Charles Chen <toshuaijun@gmail.com>
2017-10-19 20:46:46 +0800

    模拟浏览器发送GET请求，就需要使用Request对象，通过Request对象添加HTTP头，就可以伪装成浏览器请求

--- basic/do_urllib.py
+++ basic/do_urllib.py
@@ -3,7 +3,9 @@
 
 from urllib import request
 
-with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+req = request.Request('https://api.douban.com/v2/book/2129650')
+req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36')
+with request.urlopen(req) as f:
     data = f.read()
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
