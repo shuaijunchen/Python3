80049922a2b360eaa01784b107c30843aa6eff03
Charles Chen <toshuaijun@gmail.com>
2017-10-19 20:11:01 +0800

    repeat() 负责把一个元素无限重复下去，不过如果提供第二个参数就可以指定重复次数

--- basic/do_itertools.py
+++ basic/do_itertools.py
@@ -7,6 +7,11 @@ import itertools
 # for n in natuals:
 #    print(n)
 
-cs = itertools.cycle('ABC')
-for c in cs:
-    print(c)
+# cs = itertools.cycle('ABC')
+# for c in cs:
+#    print(c)
+
+# repeat() 负责把一个元素无限重复下去，不过如果提供第二个参数就可以指定重复次数
+ns = itertools.repeat('A', 3)
+for n in ns:
+    print(n)
