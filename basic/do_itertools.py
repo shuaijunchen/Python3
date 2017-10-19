a2ddb807f6a4b880edb510b8704c1676b295fe77
Charles Chen <toshuaijun@gmail.com>
2017-10-19 20:17:13 +0800

    无限序列只有在for迭代时才会无限迭代下去，通常我们可以通过takewhile()等函数根据条件来判断截取出一个有序的序列

--- basic/do_itertools.py
+++ basic/do_itertools.py
@@ -3,15 +3,19 @@
 
 import itertools
 
-# natuals = itertools.count(1)
+natuals = itertools.count(1)
 # for n in natuals:
 #    print(n)
+ns = itertools.takewhile(lambda x:x <= 10, natuals)
+print(list(ns))
 
 # cs = itertools.cycle('ABC')
 # for c in cs:
 #    print(c)
 
 # repeat() 负责把一个元素无限重复下去，不过如果提供第二个参数就可以指定重复次数
-ns = itertools.repeat('A', 3)
-for n in ns:
-    print(n)
+# ns = itertools.repeat('A', 3)
+# for n in ns:
+#    print(n)
+
+
