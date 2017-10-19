04aa1494f434db1b7a5ac144f3aa301312d8f7af
Charles Chen <toshuaijun@gmail.com>
2017-10-19 19:51:27 +0800

    sha1摘要算法

--- basic/do_hashlib.py
+++ basic/do_hashlib.py
@@ -3,12 +3,18 @@
 
 import hashlib
 
+s = 'how to use md5 in python hashlib?'
+
 md5 = hashlib.md5()
-md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
+md5.update(s.encode('utf-8'))
 print(md5.hexdigest())
 
 md5_ = hashlib.md5()
 md5_.update('how to use md5 in '.encode('utf-8'))
 md5_.update('python hashlib!'.encode('utf-8'))
 print(md5_.hexdigest())
+
+sha1 = hashlib.sha1()
+sha1.update(s.encode('utf-8'))
+print(sha1.hexdigest())
 
