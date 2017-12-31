4fe32e3179fbb105fb9ec824fd3ae24ba8603eb5
Charles Chen <toshuaijun@gmail.com>
2017-12-31 17:02:12 +0800

    表达式

---
+++ basic/exp.py
@@ -0,0 +1,6 @@
+lenght=5
+breadth=3
+
+area = lenght*breadth
+print('Area is', area)
+print('Perimeter is', 2*(lenght+breadth))
--- var.py
+++
@@ -1,9 +0,0 @@
-i = 5
-print(i)
-i=i+1
-print(i)
-
-s = '''This is a multin-line string.
-This is the second line..'''
-
-print(s)
