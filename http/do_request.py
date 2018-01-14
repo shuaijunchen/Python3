c715fd2ebeb1769a9f5248abfd06d6cfcfc8ea53
Charles Chen <toshuaijun@gmail.com>
2018-01-14 18:29:01 +0800

    request add_header

--- http/do_request.py
+++ http/do_request.py
@@ -1,16 +1,13 @@
 from urllib import request, parse
 
 url = 'https://httpbin.org/post'
-headers = {
-    'Host':'httpbin.org',
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
-}
-
 param = {
     'name':'Germey'
 }
 
 data = bytes(parse.urlencode(param), encoding='utf-8')
-req = request.Request(url=url, data=data, headers=headers, method='POST')
+req = request.Request(url=url, data=data, method='POST')
+req.add_header('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36')
+req.add_header('Host','httpbin.org')
 resp = request.urlopen(req)
 print(resp.read().decode('utf-8'))
