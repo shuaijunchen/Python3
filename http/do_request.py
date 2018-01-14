1ab0e02bc677324fcebd0379aa98e5d53d0f8699
Charles Chen <toshuaijun@gmail.com>
2018-01-14 17:52:53 +0800

    request (headers,method,data)

--- http/do_request.py
+++ http/do_request.py
@@ -1,5 +1,16 @@
-from urllib import request
+from urllib import request, parse
 
-req = request.Request('https://python.org')
+url = 'https://httpbin.org/post'
+headers = {
+    'Host':'httpbin.org',
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+}
+
+param = {
+    'name':'Germey'
+}
+
+data = bytes(parse.urlencode(param), encoding='utf-8')
+req = request.Request(url=url, data=data, headers=headers, method='POST')
 resp = request.urlopen(req)
 print(resp.read().decode('utf-8'))
