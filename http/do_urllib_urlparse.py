242fd626c05ef58585abf4406557069393623241
Charles Chen <toshuaijun@gmail.com>
2018-01-14 19:53:12 +0800

    scheme 参数

--- http/do_urllib_urlparse.py
+++ http/do_urllib_urlparse.py
@@ -1,4 +1,4 @@
 from urllib.parse import urlparse
 
-result = urlparse('http://www.baidu.com/index.html;user?id=5#comment')
-print(type(result), result)
+result = urlparse('http://www.baidu.com/index.html;user?id=5#comment', scheme='https')
+print(result)
