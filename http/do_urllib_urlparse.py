d64a4538fffc143c76acf3aef5ccad599bded21e
Charles Chen <toshuaijun@gmail.com>
2018-01-14 19:56:25 +0800

    [200~allow_fragments~

--- http/do_urllib_urlparse.py
+++ http/do_urllib_urlparse.py
@@ -1,4 +1,4 @@
 from urllib.parse import urlparse
 
-result = urlparse('http://www.baidu.com/index.html;user?id=5#comment', scheme='https')
+result = urlparse('http://www.baidu.com/index.html;user?id=5#comment', allow_fragments=False)
 print(result)
