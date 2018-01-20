f9813bd9786cd98cd25436890da32ff56bc4111f
Charles Chen <toshuaijun@gmail.com>
2018-01-20 12:30:04 +0800

    抓取网页

--- http/do_requests_get.py
+++ http/do_requests_get.py
@@ -1,6 +1,11 @@
 import requests
+import re
 
-r = requests.get('https://httpbin.org/get')
-print(type(r.text))
-print(r.json())
-print(type(r.json()))
+headers = {
+    'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
+}
+
+r = requests.get('https://www.zhihu.com/explore', headers=headers)
+pattern = re.compile('explore-feed.*?question_link.*?>(.*?)</a>',re.S)
+titles = re.findall(pattern, r.text)
+print(titles)
